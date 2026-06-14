c6244d8b8b750f413515c8918bbd4e07a02d5d11
MorthaSpandana <morthaspandana1117@gmail.com>
2026-06-14 18:35:04 +0530

    Initial project setup

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("MongoDB Connected");
+})
+.catch((err) => {
+    console.log(err);
+    console.log("MongoDB Not Connected");
+});
+
+app.get("/", (req, res) => {
+    res.send("DevBoard API Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
