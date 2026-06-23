7e1178d9a56fe98ddbe676ab7bc50abeadef1197
MorthaSpandana <morthaspandana1117@gmail.com>
2026-06-23 23:36:49 +0530

    Completed Day 8 dashboard and board integration

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,15 +1,71 @@
+import { useEffect, useState } from 'react';
+import client from '../api/client';
+
 function Dashboard() {
-  const token = localStorage.getItem('token');
+  const [boards, setBoards] = useState([]);
+  const [title, setTitle] = useState('');
+
+  useEffect(() => {
+    const fetchBoards = async () => {
+      try {
+        const response = await client.get('/boards');
+
+        setBoards(response.data);
+      } catch (error) {
+        console.log(error.response?.data);
+      }
+    };
+
+    fetchBoards();
+  }, []);
+
+  const createBoard = async(e) => {
+    e.preventDefault();
+
+    try{
+      const response = await client.post(
+        '/boards',
+        {
+          title,
+        }
+      );
+
+      console.log(response.data);
+
+      setBoards([...boards, response.data]);
+      setTitle('');
+    } catch(error){
+      console.log(error.response?.data);
+    }
+  };
 
   return (
     <div>
       <h1>Dashboard</h1>
 
-      <p>Token Exists:</p>
+      <form onSubmit={createBoard}>
+          <input
+              type="text"
+              placeholder="Enter title name"
+              value={title}
+              onChange={(e) =>
+                setTitle(e.target.value)
+              }
+          />
 
-      <pre>{token}</pre>
+          <button type="Submit">
+            Create Board
+          </button>
+
+      </form>
+      
+      {boards.map((board) => (
+        <div key={board._id}>
+          <h3>{board.title}</h3>
+        </div>
+      ))}
     </div>
   );
 }
 
-export default Dashboard;``
+export default Dashboard;
