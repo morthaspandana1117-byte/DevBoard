a70c3bd0d8e2688b950c69721c4c4d7cf5513575
MorthaSpandana <morthaspandana1117@gmail.com>
2026-07-02 23:56:56 +0530

    Configure CORS for deployment

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true,
   }),
 );
